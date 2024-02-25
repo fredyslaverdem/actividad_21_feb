6e134b85044edbd4dece56ca4644ca9aef5f3c27
Fredy Laverde <fredys.laverdem@utadeo.edu.co>
2024-02-25 18:41:53 -0500

    solución actividad

--- src/App.jsx
+++ src/App.jsx
@@ -2,54 +2,65 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [catFact, setCatFact] = useState(''); // Use more descriptive name
-  const [catImageUrl, setCatImageUrl] = useState('');
+  const [movieFact, setMovieFact] = useState('');
+  const [movieImageUrl, setMovieImageUrl] = useState('');
 
   useEffect(() => {
-    const fetchCatFact = async () => {
+    const fetchMovieFact = async () => {
       try {
-        const response = await fetch('https://catfact.ninja/fact');
+        const response = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=0dfa15e8626a34579531bd83a4f1bca6');
         const data = await response.json();
-        setCatFact(data.fact);
+
+        const randomMovie = data.results[Math.floor(Math.random() * data.results.length)];
+
+
+        const movieResponse = await fetch(`https://api.themoviedb.org/3/movie/${randomMovie.id}?api_key=0dfa15e8626a34579531bd83a4f1bca6`);
+        const movieData = await movieResponse.json();
+
+        setMovieFact(movieData.overview);
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchCatFact();
+    fetchMovieFact();
   }, []);
 
   useEffect(() => {
-    if (catFact) {
-      const generateCatImage = async () => {
+    if (movieFact) {
+      const generateMovieImage = async () => {
         try {
-          const firstWord = catFact.split(' ', 3).join(' ');
-          const response = await fetch(`https://cataas.com/cat/says/${firstWord}`);
+          const firstWord = movieFact.split(' ', 3).join(' ');
+          const response = await fetch(`https://api.pexels.com/v1/search?query=${firstWord}&per_page=1`, {
+            headers: {
+              Authorization: '9QpL1dlRYrWxvw3J6QwnnR7dNS5CQrFA0P9Nn6Hu8h6XQAsjTzkSUlik'
+            }
+          });
 
           if (response.ok) {
-            const data = await response.blob(); // Handle data as blob
-            const imageUrl = URL.createObjectURL(data); // Generate URL from blob
-            setCatImageUrl(imageUrl);
+            const data = await response.json();
+            const imageUrl = data.photos[0].src.large;
+            setMovieImageUrl(imageUrl);
           } else {
-            console.error('Error fetching cat image:', response.statusText);
+            console.error('Error fetching movie image:', response.statusText);
           }
         } catch (error) {
           console.error(error);
         }
       };
 
-      generateCatImage();
+      generateMovieImage();
     }
-  }, [catFact]);
+  }, [movieFact]);
 
   return (
     <>
-      <h1>Random Cat Fact</h1>
-      {catFact && <p>{catFact}</p>}
-      {catImageUrl && (
-        <img src={catImageUrl} alt="random cat" width={300} height={300} />
+      <h1>Dato curioso de película aleatorio</h1>
+      {movieFact && <p>{movieFact}</p>}
+      {movieImageUrl && (
+        <img src={movieImageUrl} alt="random movie" width={300} height={300} />
       )}
-      {catImageUrl && <p>Image generated based on the first word of the fact.</p>}
+      {movieImageUrl && <p>Imagen generada en base a la primera palabra del dato.</p>}
     </>
   );
 }
